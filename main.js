cf7937748c2ac542fc0328dccd78308aa6dd4b86
ferish-sharifova <farizabalabekoba199505@gmail.com>
2024-08-12 01:24:25 +0400

    array methods tasks

---
+++ main.js
@@ -0,0 +1,77 @@
+//Task1
+// const numbers=[1,2,3,4,5];
+// for (let i=0; i< numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+// //Task2
+// const frontEnd=["HTML","CSS","JS","React","Redux"];
+// for (let i=0; i<frontEnd.length; i++){
+
+//     console.log(frontEnd[i]); 
+// }
+
+
+//Task3
+// const fruits=["apple","banana","cherry","date"]
+// fruits.splice(1,2)
+// console.log(fruits);
+
+//Task4
+// const fruits=["apple","banana","cherry","date"];
+// let newfruit=prompt("meyve daxil edin");
+// fruits.push(newfruit);
+// console.log(fruits);
+
+// Task5
+
+// const fruits=["apple";"banana","cherry"];
+// console.log(fruits.indexOf("cherry") );
+
+
+// Task6
+// const fruits1=["apple","banana"]
+// const fruits2=["cheryy","date"]
+// console.log(fruits1.concat(fruits2));
+
+// Task7
+
+// const fruits=["apple","banana","cheryy"]
+// fruits.pop() 
+// console.log(fruits);
+
+// Task8
+// const fruits=["apple","banana","cheryy"]
+// console.log(fruits.join(","));
+
+// Task9
+// const frontEnd=["HTML","CSS","JS","React","Reduc"]
+// const backEnd=["Node","Express","Mongo DB"]
+// const fullStack=[]
+//  for(let i=0;i<frontEnd.length;i++){
+//     fullStack.push(frontEnd[i])
+//  }
+// for(let i=0;i<backEnd.length;i++){
+//  fullStack.push(backEnd[i])
+// }
+// console.log(fullStack);
+
+// Task10
+
+// const ages=[20,24,29,33,47,50];
+// let evenAges=[]
+// for(let i=0;i<ages.length;i++){
+//     if(ages[i]%2==0){
+//         evenAges.push(ages[i])
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
